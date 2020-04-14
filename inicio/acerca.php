642187ef3aa0a39eec0406b44c223c6e340307cd
franciscoramirezvalerio <61480638+franciscoramirezvalerio@users.noreply.github.com>
2020-04-14 10:15:09 -0500

    Mensaje
    Mensajes en encabezados.

--- inicio/acerca.php
+++ inicio/acerca.php
@@ -1,4 +1,4 @@
 <?php
 	echo "<h1>Acerca de</h1>";
-
+	echo "<h5>Mensaje</h5>";
 ?>
--- inicio/configuracion.php
+++ inicio/configuracion.php
@@ -4,5 +4,6 @@
 	echo "Nueva línea en configuración.";
 	echo "10:20pm";
 	echo "11-03-2020";
+	echo "10:13 a.m. 14-04-2020";
 
 ?>
